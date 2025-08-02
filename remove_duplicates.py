9673821b815384d8d14be294b27bbd899f983e97
Krishna <anonymous@google.com>
2025-08-02 07:49:49 +0530

    Fixed remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,5 +1,13 @@
-list_1 =[10,20,30,50,40,10]
-for i in list_1:
-    if list_1.count(i)>1:
-        list_1.remove(i)
+list_1 =[10,20,20,20,20,10]
+
+length = len(list_1)
+i = 0
+while(i<length):
+    print(i)
+    if list_1.count(list_1[i])>1:
+        list_1.remove(list_1[i])
+        length = len(list_1)
+    else:
+        i = i+1
+    
 print(list_1)
