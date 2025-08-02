f2ae3b8e84c2f8915889f3a127f29d951cd5772b
Krishna <anonymous@google.com>
2025-08-02 07:58:53 +0530

    Fixed remove_odds.py

--- remove_odds.py
+++ remove_odds.py
@@ -1,11 +1,10 @@
-list_1 = [1,7,3,5,4,8]
+list_1 = [7,4,2,6,8,9]
 l = len(list_1)
 i=0
 
 while(i<l):
     if(list_1[i] % 2 != 0):
         list_1.remove(list_1[i])
-        i = i-1
         l = len(list_1)
     else:
         i+=1
